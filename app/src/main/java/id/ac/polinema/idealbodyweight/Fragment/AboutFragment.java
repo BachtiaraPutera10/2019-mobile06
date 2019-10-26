9233967f63dea2b57dd811a1b58245971012a966
BachtiaraPutea10 <bachtiaralamsyah10@gmail.com>
2019-10-26 18:48:01 +0700

    Bundle

--- app/src/main/java/id/ac/polinema/idealbodyweight/Fragment/AboutFragment.java
+++ app/src/main/java/id/ac/polinema/idealbodyweight/Fragment/AboutFragment.java
@@ -1,4 +1,4 @@
-package id.ac.polinema.idealbodyweight;
+package id.ac.polinema.idealbodyweight.Fragment;
 
 
 import android.os.Bundle;
@@ -11,6 +11,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import id.ac.polinema.idealbodyweight.R;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -22,23 +23,13 @@ public class AboutFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_NAME = "name";
 
-
     // TODO: Rename and change types of parameters
     private String name;
-
-
 
     public AboutFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     *
-     * @return A new instance of fragment AboutFragment.
-     */
     // TODO: Rename and change types and number of parameters
     public static AboutFragment newInstance(String name) {
         AboutFragment fragment = new AboutFragment();
@@ -47,12 +38,12 @@ public class AboutFragment extends Fragment {
         fragment.setArguments(args);
         return fragment;
     }
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
             name = getArguments().getString(ARG_NAME);
-
         }
     }
 
@@ -60,7 +51,6 @@ public class AboutFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-
         View view = inflater.inflate(R.layout.fragment_about, container, false);
         TextView nameText = view.findViewById(R.id.text_name);
         nameText.setText(name);
--- app/src/main/java/id/ac/polinema/idealbodyweight/MainActivity.java
+++ app/src/main/java/id/ac/polinema/idealbodyweight/MainActivity.java
@@ -1,41 +1,40 @@
 package id.ac.polinema.idealbodyweight;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import id.ac.polinema.idealbodyweight.Fragment.AboutFragment;
+
 public class MainActivity extends AppCompatActivity {
 
-	// Deklarasikan atribut Fragment di sini
-	private AboutFragment aboutFragment;
+    // Deklarasikan atribut Fragment di sini
+    private AboutFragment aboutFragment;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        aboutFragment = AboutFragment.newInstance("Bachtiar Putera Alamsyah");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return  true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        // TODO: Tambahkan penanganan menu di sini
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		aboutFragment = aboutFragment.newInstance("Bachtiar Putera Alamsyah");
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		getMenuInflater().inflate(R.menu.menu, menu);
-		return  true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-		// TODO: Tambahkan penanganan menu di sini
-		if (item.getItemId() == R.id.menu_about) {
-			getSupportFragmentManager().beginTransaction()
-					.replace(R.id.fragment_container, aboutFragment)
-					.addToBackStack(null)
-					.commit();
-		}
-
-		return super.onOptionsItemSelected(item);
-	}
+        if (item.getItemId() == R.id.menu_about) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, aboutFragment)
+                    .addToBackStack(null)
+                    .commit();
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
