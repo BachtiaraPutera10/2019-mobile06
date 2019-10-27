e6e63110925fc5dbb1870d664b4d20a45d11aca8
BachtiaraPutea10 <bachtiaralamsyah10@gmail.com>
2019-10-27 19:49:15 +0700

    Fragment Method

--- app/src/main/java/id/ac/polinema/idealbodyweight/MainActivity.java
+++ app/src/main/java/id/ac/polinema/idealbodyweight/MainActivity.java
@@ -1,6 +1,8 @@
 package id.ac.polinema.idealbodyweight;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -8,25 +10,26 @@ import android.view.MenuItem;
 import id.ac.polinema.idealbodyweight.Fragment.AboutFragment;
 import id.ac.polinema.idealbodyweight.Fragment.BrocaIndexFragment;
 import id.ac.polinema.idealbodyweight.Fragment.MenuFragment;
+import id.ac.polinema.idealbodyweight.Fragment.ResultFragment;
 
-    public class MainActivity extends AppCompatActivity implements MenuFragment.OnFragmentInteractionListener,
-            BrocaIndexFragment.OnFragmentInteractionListener {
 
-        // Deklarasikan atribut Fragment di sini
+public class MainActivity extends AppCompatActivity implements MenuFragment.OnFragmentInteractionListener,
+    BrocaIndexFragment.OnFragmentInteractionListener, ResultFragment.OnFragmentInteractionListener {
+
         // Deklarasikan atribut Fragment di sini
         private AboutFragment aboutFragment;
         private MenuFragment menuFragment;
         private BrocaIndexFragment brocaIndexFragment;
+        private ResultFragment resultFragment;
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
-            aboutFragment = AboutFragment.newInstance("Bachtiar Putera A");
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_main);
-
-            aboutFragment = AboutFragment.newInstance("Bachtiar Putera A");
+            aboutFragment = AboutFragment.newInstance("Dini Triana Dewi");
             menuFragment = new MenuFragment();
             brocaIndexFragment = new BrocaIndexFragment();
+            resultFragment = new ResultFragment();
 
             getSupportFragmentManager().beginTransaction()
                     .replace(R.id.fragment_container, menuFragment)
@@ -36,7 +39,7 @@ import id.ac.polinema.idealbodyweight.Fragment.MenuFragment;
         @Override
         public boolean onCreateOptionsMenu(Menu menu) {
             getMenuInflater().inflate(R.menu.menu, menu);
-            return true;
+            return  true;
         }
 
         @Override
@@ -60,11 +63,20 @@ import id.ac.polinema.idealbodyweight.Fragment.MenuFragment;
 
         @Override
         public void onBodyMassIndexButtonClicked() {
-
         }
 
         @Override
         public void onCalculateBrocaIndexClicked(float index) {
+            resultFragment.setInformation(String.format("Your ideal weight is %.2f kg", index));
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, resultFragment)
+                    .commit();
+        }
 
+        @Override
+        public void onTryAgainButtonClicked(String tag) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, brocaIndexFragment)
+                    .commit();
         }
     }
